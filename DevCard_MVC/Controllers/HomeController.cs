877f4b95ce1a3a64e3b122001645c7ad27d64cdf
lenovo <lenovo@DESKTOP-SKNTUT6>
2025-07-14 10:00:37 +0330

    Add project files.

---
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
